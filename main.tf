770692c4de08ecc4266e7db8f706aa80a260a975
ayvy <silvestris76diary-ayvy@yahoo.co.jp>
2020-07-05 15:30:39 +0900

    fix2

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 variable_access_key" {}
 variable_secret_key" {}
 
-Provider "aws" {
+provider "aws" {
 	access_key = var.aws_access_key
 	secret_key = var.aws_secret_key
 	region = "ap-northeast-1"
 }
 
-resource " aws_vpc" "vpctest" {
+resource "aws_vpc" "vpctest" {
 	cidr_block = "172.16.0.0/24"
 	tags = {
 		name = "vpctest"
